76dc1840e2ef329a90ca5dbb8af80a63c007796a
Astambalitse Yim <elimiriel@gmail.com>
2025-03-27 18:34:16 +0900

    (recommit: dept02 is 2nd, emp is 3rd(singleton))

---
+++ JSP/dept02/src/main/java/dept02/dept02/com/dept02/DeptDTO.java
@@ -0,0 +1,26 @@
+package dept02.dept02.com.dept02;
+//DB gathering Object
+
+public class DeptDTO {
+    private int deptno;
+    private String dname;
+    private String location;
+    public int getDeptno() {
+        return deptno;
+    }
+    public void setDeptno(int deptno) {
+        this.deptno = deptno;
+    }
+    public String getDname() {
+        return dname;
+    }
+    public void setDname(String dname) {
+        this.dname = dname;
+    }
+    public String getLocation() {
+        return location;
+    }
+    public void setLocation(String location) {
+        this.location = location;
+    }
+}
